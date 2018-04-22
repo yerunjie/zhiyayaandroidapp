55484e20c8aaf68d0022866e9b543ab1b1aaba70
yerunjie <1102266271@qq.com>
2018-04-22 12:00:14 +0800

    import code

---
+++ app/src/main/java/com/zhiyaya/zhiyayaapplication/push/MyJPushMessageReceiver.java
@@ -0,0 +1,30 @@
+package com.zhiyaya.zhiyayaapplication.push;
+
+import android.content.Context;
+import cn.jpush.android.api.JPushMessage;
+import cn.jpush.android.service.JPushMessageReceiver;
+
+public class MyJPushMessageReceiver extends JPushMessageReceiver {
+
+    @Override
+    public void onTagOperatorResult(Context context, JPushMessage jPushMessage) {
+        TagAliasOperatorHelper.getInstance().onTagOperatorResult(context,jPushMessage);
+        super.onTagOperatorResult(context, jPushMessage);
+    }
+    @Override
+    public void onCheckTagOperatorResult(Context context,JPushMessage jPushMessage){
+        TagAliasOperatorHelper.getInstance().onCheckTagOperatorResult(context,jPushMessage);
+        super.onCheckTagOperatorResult(context, jPushMessage);
+    }
+    @Override
+    public void onAliasOperatorResult(Context context, JPushMessage jPushMessage) {
+        TagAliasOperatorHelper.getInstance().onAliasOperatorResult(context,jPushMessage);
+        super.onAliasOperatorResult(context, jPushMessage);
+    }
+
+    @Override
+    public void onMobileNumberOperatorResult(Context context, JPushMessage jPushMessage) {
+        TagAliasOperatorHelper.getInstance().onMobileNumberOperatorResult(context,jPushMessage);
+        super.onMobileNumberOperatorResult(context, jPushMessage);
+    }
+}
